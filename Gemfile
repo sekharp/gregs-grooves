798c758b6771d9714e8c4eec18a13a6b3281ba45
Sekhar Paladugu <sekharp@gmail.com>
2018-05-19 16:25:27 -0600

    add record create controller method, adjust enum code, rework seed file

--- Gemfile
+++ Gemfile
@@ -28,6 +28,8 @@ gem 'bootsnap', '>= 1.1.0', require: false
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
 # gem 'rack-cors'
 
+gem 'faker'
+
 group :development, :test do
   # Use sqlite3 as the database for Active Record
   gem 'sqlite3'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |r|
+  Record.create(
+    artist: Faker::Hipster.word,
+    title: Faker::Hipster.sentence.truncate(50),
+    year: rand(1958..2018),
+    condition: 1
+  )
+end
