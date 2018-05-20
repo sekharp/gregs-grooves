5bb9c86291e256350c249ce868833cddc8573eaf
Sekhar Paladugu <sekharp@gmail.com>
2018-05-19 21:05:12 -0600

    build delete functionality into react bootstrap table with backend api

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope '/api' do
+    resources :records
+  end
+
   get '*path', to: "application#fallback_index_html", constraints: ->(request) do
     !request.xhr? && request.format.html?
   end
